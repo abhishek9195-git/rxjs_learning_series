17c163e66ebd6a9451bf4ff6ff36178273c6f640
abhishek <abhishek.km2010@gmail.com>
2026-07-09 18:34:50 +0530

    feat: observer updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,13 @@
+import { AsyncPipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+
+const observer = {
+  next: (value: number) => {console.log('==> Value: ', value)},
+  error: (err: any) => {console.log(err)},
+  complete: () => {console.log('Completed')}
+}
 
 @Component({
   selector: 'app-root',
@@ -8,4 +16,28 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
+
+  subscription!: Subscription
+  counter = signal(0)
+
+  obs$ = new Observable<number>(observer => {
+    let i = 0
+    const interval = setInterval(() => {
+      observer.next(i++)
+    }, 1000);
+
+    return {
+      unsubscribe: () => {
+        clearInterval(interval)
+      }
+    }
+  })
+
+  start() {
+    this.subscription = this.obs$.subscribe(observer)
+  }
+
+  stop() {
+    this.subscription.unsubscribe()
+  }
 }
