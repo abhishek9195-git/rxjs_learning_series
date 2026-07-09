dfbd9e898a2e622a03dc65f715dff3c40a3dfa49
abhishek <abhishek.km2010@gmail.com>
2026-07-09 14:52:56 +0530

    feat: updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
+import { AsyncPipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { interval, Observable, of, Subscription, take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,31 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
+
+  subscription!: Subscription
+  counter$ = signal(0)
+
+  obs$ = new Observable<number>((observer) => {
+    let counter = 1;
+    const interval = setInterval(() => {
+      observer.next(counter++)
+    }, 1000);
+
+    return {
+      unsubscribe: () => {
+        console.log('==> clear interval', interval)
+        clearInterval(interval)
+      }
+    }
+  })
+
+  startTimer() {
+    this.subscription = this.obs$.subscribe((value) => {
+      this.counter$.set(value)
+    })
+  }
+
+  stopTimer() {
+    this.subscription?.unsubscribe()
+  }
 }
