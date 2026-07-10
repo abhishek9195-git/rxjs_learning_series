c311c31ee53d250fcc9df04d6e97f8f019a7cbc8
abhishek <abhishek.km2010@gmail.com>
2026-07-10 17:35:39 +0530

    updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { interval, reduce, take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,11 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
+
+  ngOnInit() {
+    interval(1000).pipe(
+      take(5),
+      reduce((acc, e, i) => acc += e, 0)
+    ).subscribe({next: v => console.log(v)})
+  }
 }
