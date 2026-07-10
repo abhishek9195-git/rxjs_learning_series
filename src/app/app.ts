3924a4501d08421173ca0fd06087ae24d76c1488
abhishek <abhishek.km2010@gmail.com>
2026-07-10 12:27:50 +0530

    feat: updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,19 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
+
+  sub$ = new BehaviorSubject<string>('Initial value')
+
+  ngOnInit() {
+    this.sub$.subscribe({
+      next: (value) => {console.log('Subscriber 1: ', value)}
+    })
+
+    this.sub$.next('A')
+
+    this.sub$.subscribe({
+      next: (value) => {console.log('Subscriber 2: ', value)}
+    })
+  }
+
 }
