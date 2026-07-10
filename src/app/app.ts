54714d2feec5f60298d833332bb579c4732d92dd
abhishek <abhishek.km2010@gmail.com>
2026-07-10 16:26:29 +0530

    updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AsyncSubject, EMPTY, finalize, iif, interval, map, mergeMap, of, Subject, take, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,27 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
+
+  stream = new AsyncSubject<string>()
+
+  ngOnInit() {
+    const fruits = ['apple', 'banana', 'orange', 'guava', 'grapes']
+
+    this.stream.subscribe({
+      next: (v) => {
+        console.log(v);
+      },
+      complete:() => {
+        console.log('==> Completed');
+      }
+    })
+
+
+    interval(1000).pipe(
+      take(fruits.length),
+      tap((i) => this.stream.next(fruits[i]))
+    ).subscribe({
+      complete: () => this.stream.complete()
+    })
+  }
 }
