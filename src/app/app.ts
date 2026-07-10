185d8a6370d3994481f7398a4dce098588f17028
abhishek <abhishek.km2010@gmail.com>
2026-07-10 16:48:30 +0530

    updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EMPTY, iif, interval, map, mergeMap, of, Subject, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,21 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
+
+  stream = new Subject<string>()
+  fruits = ['Apple', 'Banana', 'Orange', 'Guava', 'Grapes']
+
+  ngOnInit() {
+
+    this.stream.subscribe({next: (v) => console.log(v)})
+
+    interval(1000).pipe(
+      mergeMap((i) => iif(
+        () => i < this.fruits.length, 
+        of(this.fruits[i]).pipe(tap(fruit => this.stream.next(fruit))) , 
+        EMPTY)
+      )
+    ).subscribe()
+
+  }
 }
