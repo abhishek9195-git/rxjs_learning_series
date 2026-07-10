08c9969b38fc7d7d7e2e6734040a78da592ffe62
abhishek <abhishek.km2010@gmail.com>
2026-07-10 15:36:22 +0530

    updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReplaySubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,19 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
+
+  stream = new ReplaySubject<number>(3)
+
+  ngOnInit() {
+    let i = 0;
+    setInterval(() => {
+      this.stream.next(i++);
+    }, 1000);
+  }
+
+  printStream() {
+    this.stream.subscribe({
+      next: (v) => console.log('==> v', v)
+    })
+  }
 }
