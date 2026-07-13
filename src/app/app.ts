1d275fb178eace399d88486ba4d282dff8bfc769
abhishek <abhishek.km2010@gmail.com>
2026-07-13 14:03:49 +0530

    updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
 import { RouterOutlet } from '@angular/router';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +11,27 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
+
+    private readonly http = inject(HttpClient);
+    private readonly BASE_URL = 'https://dummyjson.com'
+    
+    category = signal('beauty');
+    category$ = toObservable(this.category);
+    products$ = this.http.get(`${this.BASE_URL}/products/category/${this.category()}`)
+
+    ngOnInit() {
+
+        combineLatest({
+            category: this.category$,
+            products: this.products$
+        })
+        .subscribe({
+            next: (response) => {
+                console.log(response)
+            } 
+        })
+    }
+
+
+
 }
