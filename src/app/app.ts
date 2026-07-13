c4e28a0768eaf8ff3fbcf034f9ad025815d56fb8
abhishek <abhishek.km2010@gmail.com>
2026-07-13 14:00:55 +0530

    updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { forkJoin, Subject } from 'rxjs';
+
+interface IUser {
+  id: number,
+  name: string
+}
 
 @Component({
   selector: 'app-root',
@@ -8,4 +15,25 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
+
+  stream = new Subject();
+  private readonly http = inject(HttpClient);
+  BASE_URL = 'https://jsonplaceholder.typicode.com'
+
+  ngOnInit() {
+
+    const user$ = this.http.get(`${this.BASE_URL}/users/1`)
+    const todo$ = this.http.get(`${this.BASE_URL}/todos/1`)
+
+    forkJoin({
+      user: user$,
+      todo: todo$
+    })
+    .subscribe({
+      next: ({user, todo}) => {
+        console.log(user);
+        console.log(todo);
+      }
+    })
+  }
 }
