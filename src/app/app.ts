860b802ecc2d1ea898f8aa3dfe80074304d5d4a0
abhishek <abhishek.km2010@gmail.com>
2026-07-13 10:34:58 +0530

    updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { concatMap, from } from 'rxjs';
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com'
+type IUser = {
+  id: string,
+  name: string 
+}
 
 @Component({
   selector: 'app-root',
@@ -8,4 +16,14 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
+
+  private readonly http = inject(HttpClient)
+
+  ngOnInit() {
+    from([1, 2, 3, 4, 5]).pipe(
+      concatMap((userId: number) => this.http.get<IUser>(`${BASE_URL}/users/${userId}`))
+    ).subscribe({
+      next: v => console.log(v.name)
+    })
+  }
 }
