8b13eb8824892c92f0fd469f7dd5aaa8397b2043
abhishek9195-git <abhishek9195@outlook.com>
2026-07-16 15:41:15 +0530

    Refactor App component to use reactive forms

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,37 +1,46 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { toObservable } from '@angular/core/rxjs-interop';
+import { form, FormField } from '@angular/forms/signals';
 import { RouterOutlet } from '@angular/router';
-import { combineLatest } from 'rxjs';
+import { combineLatest, map, tap } from 'rxjs';
+
+interface IProduct {
+  id: number,
+  title: string,
+  description: string,
+  category: string
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormField],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
 
-    private readonly http = inject(HttpClient);
-    private readonly BASE_URL = 'https://dummyjson.com'
-    
-    category = signal('beauty');
-    category$ = toObservable(this.category);
-    products$ = this.http.get(`${this.BASE_URL}/products/category/${this.category()}`)
+  private readonly http = inject(HttpClient);
+  private readonly BASE_URL = 'https://dummyjson.com'
 
-    ngOnInit() {
+  _model = signal({ category: 'beauty' });
+  form = form(this._model)
 
-        combineLatest({
-            category: this.category$,
-            products: this.products$
-        })
-        .subscribe({
-            next: (response) => {
-                console.log(response)
-            } 
-        })
-    }
+  category$ = toObservable(this._model).pipe(map(v => v.category))
+  products$ = this.http.get<{ products: IProduct[] }>(`${this.BASE_URL}/products`).pipe(map(v => v.products))
 
+  ngOnInit() {
 
+    combineLatest({
+      category: this.category$,
+      products: this.products$
+    }).pipe(
+      map(({category, products}) => {
+        return products.filter((product) => product.category === category)
+      })
+    ).subscribe(products => {
+      console.log(products);
+    });
 
+  }
 }
